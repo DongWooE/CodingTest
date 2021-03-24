a9eb1d597b3753bc306578415d278dbdef70954e
DongWooE <ehddn977@naver.com>
2021-03-24 16:05:06 +0900

    bronze

---
+++ BOJ_2908.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int function(string s) {
+	string temp;
+	string::iterator it = s.end() - 1;
+	while (1) {
+		temp += *it;
+		if (it == s.begin()) break;
+		it--;
+	}
+	return stoi(temp);
+}
+
+int main() {
+
+	string A, B;
+	cin >> A >> B;
+	int num1 = function(A);
+	int num2 = function(B);
+
+	if (num1 > num2) cout << num1;
+	else cout << num2;
+}
+
