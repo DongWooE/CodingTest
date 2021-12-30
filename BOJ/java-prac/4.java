3017f48a1edc150b7e102e979557abe5e35adc41
DongWooE <ehddn977@naver.com>
2021-12-30 11:23:02 +0900

    [ADD] 4.java

---
+++ BOJ/java-prac/4.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public void solution(ArrayList<String> s) {
+        for(String x : s){
+            StringBuilder sb = new StringBuilder(x);
+            System.out.println(sb.reverse().toString());
+        }
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        ArrayList<String> l = new ArrayList<String>();
+        while(t > 0){
+            String s = scan.next();
+            l.add(s);
+            t--;
+        }
+        main.solution(l);
+    }
+}
+//public class Main {
+//    public void solution(String s) {
+//        int t = s.length();
+//        while(t > 0){
+//            System.out.print(s.charAt(t-1));
+//            t--;
+//        }
+//        System.out.println();
+//    }
+//    public static void main(String[] args) {
+//        Main main = new Main();
+//        Scanner scan = new Scanner(System.in);
+//        int t = scan.nextInt();
+//        while(t > 0){
+//            String s = scan.next();
+//            main.solution(s);
+//            t--;
+//        }
+//    }
+//}
