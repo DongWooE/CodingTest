3357029ea9ec5061e29b1e7de389e2252419814b
DongWooE <ehddn977@naver.com>
2022-01-18 23:01:29 +0900

    [ADD] 18.java

---
+++ BOJ/java-prac/18.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class code19 {
+    public static boolean solution(int number){
+        int[] array = new int [number+1];
+        if(number == 1) return false;
+        for(int i = 2; i < number; i++){
+            if(array[number] != 0) break;
+            if(array[i] == 0){
+                for(int j = i; j <= number; j += i){
+                    array[j] = 1;
+                }
+            }
+        }
+        return array[number] == 0;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int array [] = new int [N];
+        int index = 0;
+        while(N-- > 0){
+            int number = scan.nextInt();
+            StringBuilder stringBuilder = new StringBuilder(String.valueOf(number));
+            stringBuilder = stringBuilder.reverse();
+            number = Integer.parseInt(stringBuilder.toString());
+            if(solution(number)) {
+                array[index++] = number;
+            }
+        }
+        for(int cur : array){
+            if(cur != 0) System.out.print(cur + " ");
+        }
+    }
+}
